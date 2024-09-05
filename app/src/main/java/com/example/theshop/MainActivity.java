e0b057bf0579f80a354f1ba67625043aeef1b0bd
Ditjo <mads.ditjo@gmail.com>
2024-09-05 13:10:52 +0200

    Added Main view layout

--- app/src/main/java/com/example/theshop/MainActivity.java
+++ app/src/main/java/com/example/theshop/MainActivity.java
@@ -1,24 +1,60 @@
 package com.example.theshop;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ImageView iv_mainMenu, iv_basket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initGui();
+        setGuiListeners();
+    }
+
+    void initGui(){
+        drawerLayout = findViewById(R.id.main);
+        iv_mainMenu = findViewById(R.id.iv_mainMenu);
+        iv_basket = findViewById(R.id.iv_basket);
+    }
+
+    void setGuiListeners(){
+        iv_mainMenu.setOnClickListener(x -> mainMenuDrawer());
+        iv_basket.setOnClickListener(x -> basketDrawer());
+    }
+
+    void mainMenuDrawer(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+    }
+
+    void basketDrawer(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.END)){
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else {
+            drawerLayout.openDrawer(GravityCompat.END);
+        }
     }
 }
